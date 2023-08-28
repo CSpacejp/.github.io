49676009e4cc84a6d5667c41d427ddacb3d7db3d
hari369-cmd <hari.harinaz@gmail.com>
2023-08-28 19:46:48 +0900

    modified:   assets/js/translation.js 	modified:   assets/translation.json 	modified:   index.html

--- assets/js/translation.js
+++ assets/js/translation.js
@@ -4,6 +4,13 @@ document.addEventListener("DOMContentLoaded", async function () {
     const learn_more_button = document.getElementById("learn__more__button");
     const about_section_title = document.getElementById("about__section__title");
 
+    const about_title_1 = document.getElementById("about__title__1");
+    const about_title_2 = document.getElementById("about__title__2");
+    const about_title_3 = document.getElementById("about__title__3");
+    const about_description_1 = document.getElementById("about__description__1");
+    const about_description_2 = document.getElementById("about__description__2");
+    const about_description_3 = document.getElementById("about__description__3");
+
     const translations = await fetch("https://raw.githubusercontent.com/CSpacejp/CSpacejp.github.io/main/assets/translation.json").then(response => response.json());
   
     let currentLanguage = "en"; // Default language
@@ -12,6 +19,13 @@ document.addEventListener("DOMContentLoaded", async function () {
       home_edu_description.textContent = translations[language].home__edu__description;
       learn_more_button.textContent = translations[language].learn__more__button;
       about_section_title.innerHTML = translations[language].about__section__title;
+
+      about_title_1.textContent = translations[language].about__title__1;
+      about_title_2.textContent = translations[language].about__title__2;
+      about_title_3.textContent = translations[language].about__title__3;
+      about_description_1.textContent = translations[language].about__description__1;
+      about_description_2.textContent = translations[language].about__description__2;
+      about_description_3.textContent = translations[language].about__description__3;
     }
   
     translateButton.addEventListener("click", function () {
