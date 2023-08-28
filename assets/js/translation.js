84a16d64ed43c3fe11b56546c0406d9063df1576
hari369-cmd <hari.harinaz@gmail.com>
2023-08-28 19:02:34 +0900

    modified:   assets/js/translation.js 	modified:   assets/translation.json 	modified:   index.html

--- assets/js/translation.js
+++ assets/js/translation.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", async function () {
     const translateButton = document.getElementById("translateButton");
     const home_edu_description = document.getElementById("home__edu__description");
-    
+    const learn_more_button = document.getElementById("learn__more__button");
+    const about_section_title = document.getElementById("about__section__title");
 
     const translations = await fetch("https://raw.githubusercontent.com/CSpacejp/CSpacejp.github.io/main/assets/translation.json").then(response => response.json());
   
@@ -9,6 +10,8 @@ document.addEventListener("DOMContentLoaded", async function () {
   
     function updateContent(language) {
       home_edu_description.textContent = translations[language].home__edu__description;
+      learn_more_button.textContent = translations[language].learn__more__button;
+      about_section_title = translations[language].about__section__title;
     }
   
     translateButton.addEventListener("click", function () {
