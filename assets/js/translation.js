84ab69c8a47df3dbf4eed811d67c0c1ee246f894
hari369-cmd <hari.harinaz@gmail.com>
2023-08-28 18:32:30 +0900

    modified:   assets/css/styles.css 	new file:   assets/js/translation.js 	new file:   assets/translation.json 	modified:   index.html

---
+++ assets/js/translation.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", async function () {
+    const translateButton = document.getElementById("translateButton");
+    const home_edu_description = document.getElementById("home__edu__description");
+    
+
+    const translations = await fetch("https://raw.githubusercontent.com/CSpacejp/CSpacejp.github.io/main/translations.json").then(response => response.json());
+  
+    let currentLanguage = "en"; // Default language
+  
+    function updateContent(language) {
+      home_edu_description.textContent = translations[language].home__edu__description;
+    }
+  
+    translateButton.addEventListener("click", function () {
+      currentLanguage = currentLanguage === "en" ? "ja" : "en";
+      updateContent(currentLanguage);
+    });
+  
+    // Initialize content with the default language
+    updateContent(currentLanguage);
+  });
