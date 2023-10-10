fc13cac5a4c11c4bcd982357037d22f4db8285bd
hari369-cmd <hari.harinaz@gmail.com>
2023-10-11 01:27:15 +0900

    website_update_Oct11

--- assets/js/translation.js
+++ assets/js/translation.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", async function () {
     const translateButton = document.getElementById("translateButton");
+    const notification_id = document.getElementById("notification__id"); //temp
+    const donateButton = document.getElementById("donateButton"); //temp
     const home_edu_description = document.getElementById("home__edu__description");
     const learn_more_button = document.getElementById("learn__more__button");
     const about_section_title = document.getElementById("about__section__title");
@@ -23,6 +25,8 @@ document.addEventListener("DOMContentLoaded", async function () {
     let currentLanguage = "en"; // Default language
   
     function updateContent(language) {
+      notification_id.textContent = translations[language].notification__id;
+      donateButton.textContent = translations[language].donateButton;
       home_edu_description.textContent = translations[language].home__edu__description;
       learn_more_button.textContent = translations[language].learn__more__button;
       about_section_title.innerHTML = translations[language].about__section__title;
