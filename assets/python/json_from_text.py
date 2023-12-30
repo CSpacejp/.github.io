b11646419326dd34e318fb57442cabfccf764638
hari369-cmd <hari.harinaz@gmail.com>
2023-12-31 02:21:27 +0900

    update-post

--- assets/python/json_from_text.py
+++ assets/python/json_from_text.py
@@ -5,7 +5,7 @@ import json
 #from watchdog.events import FileSystemEventHandler
 
 # Choose the language
-language = "en"
+language = "jp"
 
 # Path to the directory containing text files
 text_files_directory = "../updates/{}/".format(language)
